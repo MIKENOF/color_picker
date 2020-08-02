4ea0130c6ba7b9efacb680ac2156c3d8913f3222
MIKENOF <jairo-lara@live.com>
2020-08-01 21:09:28 -0500

    Agregado historial en ListView.builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import './src/models/colores.dart';
 import 'package:flutter/material.dart';
+
+import './src/models/resistencia.dart';
+import './src/models/colores.dart';
 
 void main() => runApp(MyApp());
 
@@ -25,8 +27,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   static List<Colores> barra1 = Colores.getColoresVal();
   static List<Colores> barra2 = Colores.getColoresVal();
-  static List<Colores> barraMultiplicador = Colores.getColoresMulti();
-  static List<Colores> barraTolerancias = Colores.getColoresTol();
+  static List<Colores> barraMultiplicadorList = Colores.getColoresMulti();
+  static List<Colores> barraToleranciasList = Colores.getColoresTol();
+
+  ScrollController _scrollController = new ScrollController();
 
   int valor1 = 0;
   int valor2 = 0;
@@ -34,120 +38,269 @@ class _MyHomePageState extends State<MyHomePage> {
   int tolIndex = 0;
   double valFin = 0.0;
   String prefijo = '';
-  String texto = '0 Ω';
+  String valRes = '0.00 Ω ±1%';
+
+  List<Resistencia> listaResistencias = [
+    Resistencia(
+        indexBarra1: 2,
+        indexBarra2: 2,
+        indexBarraMult: 1,
+        indexBarraTol: 6,
+        valor: '220.00 Ω ±5%'),
+    Resistencia(
+        indexBarra1: 1,
+        indexBarra2: 0,
+        indexBarraMult: 2,
+        indexBarraTol: 6,
+        valor: '1.00 KΩ ±5%'),
+    Resistencia(
+        indexBarra1: 1,
+        indexBarra2: 0,
+        indexBarraMult: 3,
+        indexBarraTol: 6,
+        valor: '10.00 KΩ ±5%'),
+  ];
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        // _agregar10();
+
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        child: const Text('showModalBottomSheet'),
-        onPressed: () {
-          showModalBottomSheet<void>(
-            context: context,
-            builder: (BuildContext context) {
-              return Container(
-                height: 300,
-                color: Colors.amber,
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Colores Resistencias'),
+      ),
+      body: SafeArea(
+        child: Container(
+          alignment: Alignment.topCenter,
+          padding: EdgeInsets.all(10.0),
+          child: ListView.separated(
+            separatorBuilder: (context, index) {
+              return Divider();
+            },
+            // reverse: true,
+            controller: _scrollController,
+            itemCount: listaResistencias.length,
+            itemBuilder: (context, index) {
+              return Dismissible(
+                background: Container(
+                  padding: EdgeInsets.only(left: 12.0),
+                  color: Colors.blueGrey,
+                  child: Row(
                     children: <Widget>[
-                      const Text('Modal BottomSheet'),
-                      RaisedButton(
-                        child: const Text('Close BottomSheet'),
-                        onPressed: () => Navigator.pop(context),
+                      Icon(Icons.delete),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Text('Eliminar'),
                       ),
-                      agregarResistencia(),
+                    ],
+                  ),
+                  alignment: Alignment.centerLeft,
+                ),
+                onDismissed: (direction) {
+                  setState(() {
+                    listaResistencias.removeAt(index);
+                  });
+                  Scaffold.of(context).showSnackBar(SnackBar(
+                    duration: Duration(milliseconds: 200),
+                    content: Text("Resistencia Eliminado"),
+                  ));
+                },
+                key: UniqueKey(),
+                child: Container(
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black),
+                              color:
+                                  barra1[listaResistencias[index].indexBarra1]
+                                      .color,
+                            ),
+                            height: 50,
+                            width: 20,
+                          ),
+                          Container(height: 50, width: 2),
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black),
+                              color:
+                                  barra2[listaResistencias[index].indexBarra2]
+                                      .color,
+                            ),
+                            height: 50,
+                            width: 20,
+                            // color: barra2[listaResistencias[index].indexBarra2].color,
+                          ),
+                          Container(height: 50, width: 2),
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black),
+                              color: barraMultiplicadorList[
+                                      listaResistencias[index].indexBarraMult]
+                                  .color,
+                            ),
+                            height: 50,
+                            width: 20,
+                          ),
+                          Container(height: 50, width: 2),
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black),
+                              color: barraToleranciasList[
+                                      listaResistencias[index].indexBarraTol]
+                                  .color,
+                            ),
+                            height: 50,
+                            width: 20,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Text(
+                                'Valor: ${listaResistencias[index].valor}'),
+                          ),
+                        ],
+                      ),
+                      // Divider(),
                     ],
                   ),
                 ),
               );
             },
-          );
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          valor1 = 0;
+          valor2 = 0;
+          mIndex = 0;
+          tolIndex = 0;
+          valFin = 0.0;
+          prefijo = '';
+          valRes = '0.00 Ω ±1%';
+          displayBottomSheet(context);
         },
       ),
     );
   }
 
-  Widget bottomSheetSimple(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        child: const Text('showModalBottomSheet'),
-        onPressed: () {},
-      ),
-    );
-  }
-
-  displayBottomSheet() {
-    double heightOfModalBottomSheet = 100;
-    int value1 = 0;
+  displayBottomSheet(BuildContext context) {
+    double heightOfModalBottomSheet = 250;
     showModalBottomSheet(
-      backgroundColor: Colors.blueGrey,
+      isScrollControlled: true,
       context: context,
       builder: (context) {
-        return StatefulBuilder(
-          builder: (BuildContext context,
-              StateSetter setStateUp /*You can rename this!*/) {
-            return agregarResistencia();
-          },
+        return Container(
+          height: heightOfModalBottomSheet,
+          child: Scaffold(
+            body: StatefulBuilder(
+              builder: (BuildContext context,
+                  StateSetter setStateBuilder /*You can rename this!*/) {
+                return Column(
+                  children: <Widget>[
+                    agregarResistencia(context, setStateBuilder),
+                  ],
+                );
+              },
+            ),
+          ),
         );
       },
     );
   }
 
-  Widget agregarResistencia() {
-    return Column(
-      children: <Widget>[
-        _textos(),
-        Container(
-          padding: EdgeInsets.all(10.0),
-          child: Row(
+  Widget agregarResistencia(BuildContext context, Function setStateBuilder) {
+    return Container(
+      padding: EdgeInsets.only(top: 15.0),
+      child: Column(
+        children: <Widget>[
+          textos(),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(child: barraUno(setStateBuilder)),
+                Expanded(child: barraDos(setStateBuilder)),
+                Expanded(child: barraMultiplicador(setStateBuilder)),
+                Expanded(child: barraTolerancias(setStateBuilder))
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 20.0),
+          ),
+          Row(
             children: <Widget>[
-              Expanded(child: _barraUno()),
-              Expanded(child: _barraDos()),
-              Expanded(child: _barraMultiplicador()),
-              Expanded(child: _barraTolerancias()),
+              Padding(
+                padding: const EdgeInsets.only(left: 12.0, right: 12.0),
+                child: Text('Valor: $valRes'),
+              ),
+              Expanded(child: Divider()),
+              Padding(
+                padding: const EdgeInsets.only(right: 12.0, left: 12.0),
+                child: RaisedButton(
+                  onPressed: () {
+                    setState(() {
+                      if (valor1 == 0 && valor2 == 0) {
+                        Scaffold.of(context).showSnackBar(SnackBar(
+                            duration: Duration(milliseconds: 600),
+                            content: Text('Valores Inválidos')));
+                        return;
+                      } else {
+                        listaResistencias.insert(
+                          0,
+                          Resistencia(
+                            indexBarra1: valor1,
+                            indexBarra2: valor2,
+                            indexBarraMult: mIndex,
+                            indexBarraTol: tolIndex,
+                            valor: valRes,
+                          ),
+                        );
+                        Navigator.pop(context);
+                        _scrollController.animateTo(
+                            // _scrollController.position.maxScrollExtent + 66,
+                            0.0,
+                            duration: Duration(milliseconds: 1000),
+                            curve: Curves.easeOutCirc);
+                      }
+                    });
+                  },
+                  child: Text('Guardar'),
+                ),
+              )
             ],
           ),
-        ),
-        Container(
-            child: Row(
-          children: <Widget>[
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Container(child: Text('$texto')),
-              ),
-            ),
-            Expanded(
-              child: FlatButton(
-                onPressed: () {
-                  setState(() {});
-                },
-                child: Row(
-                  children: <Widget>[
-                    Text('Guardar'),
-                    SizedBox(width: 30.0),
-                    Icon(Icons.save),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        )),
-      ],
+        ],
+      ),
     );
   }
 
-  String calcularResistencia() {
-    String tol = barraTolerancias[tolIndex].tolerancia;
+  calcularResistencia() {
+    String tol = barraToleranciasList[tolIndex].tolerancia;
     String valorRes = '';
-    int val;
     valorRes = valor1.toString() + valor2.toString();
     // val = int.parse(valorRes);
     double valordouble = double.parse(valorRes);
-    double valor = valordouble * barraMultiplicador[mIndex].multiplicador;
+    double valor = valordouble * barraMultiplicadorList[mIndex].multiplicador;
     print(valor.toStringAsFixed(2));
     if (valor ~/ 1000 > 0) {
       valFin = valor / 1000;
@@ -164,10 +317,11 @@ class _MyHomePageState extends State<MyHomePage> {
       valFin = valor;
       prefijo = 'Ω $tol';
     }
-    return '${valFin.toStringAsFixed(2)} $prefijo';
+    valRes = '${valFin.toStringAsFixed(2)} $prefijo';
+    // valorInt * barraMultiplicador[multiplicador].multiplicador.toInt());
   }
 
-  Widget _textos() {
+  Widget textos() {
     return Container(
       padding: EdgeInsets.only(top: 10.0, left: 10.0, right: 10.0),
       child: Row(
@@ -190,8 +344,7 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget _barraUno( ) {
-    int _index = 0;
+  Widget barraUno(Function setStateBuilder) {
     return SizedBox(
       height: 80, // card height
       width: 80,
@@ -199,17 +352,16 @@ class _MyHomePageState extends State<MyHomePage> {
         itemCount: barra1.length,
         scrollDirection: Axis.vertical,
         controller: PageController(viewportFraction: 0.7, keepPage: true),
-        onPageChanged: (int index) => setState(() {
-          _index = index;
+        onPageChanged: (int index) => setStateBuilder(() {
           valor1 = index;
-          texto  = calcularResistencia();
+          calcularResistencia();
         }),
         itemBuilder: (context, i) {
           return Container(
             child: Card(
               color: barra1[i].color,
               shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(1)),
+                  borderRadius: BorderRadius.circular(10)),
               child: Center(
                 child: Text(
                   barra1[i].nombre,
@@ -226,8 +378,7 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget _barraDos( ) {
-    int _index = 0;
+  Widget barraDos(Function setStateBuilder) {
     return SizedBox(
       height: 80, // card height
       width: 80,
@@ -235,10 +386,9 @@ class _MyHomePageState extends State<MyHomePage> {
         itemCount: barra2.length,
         scrollDirection: Axis.vertical,
         controller: PageController(viewportFraction: 0.7),
-        onPageChanged: (int index) => setState(() {
-          _index = index;
+        onPageChanged: (int index) => setStateBuilder(() {
           valor2 = index;
-          texto  = calcularResistencia();
+          calcularResistencia();
         }),
         itemBuilder: (context, i) {
           return Container(
@@ -262,29 +412,27 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget _barraMultiplicador( ) {
-    int _index = 0;
+  Widget barraMultiplicador(Function setStateBuilder) {
     return SizedBox(
       height: 80, // card height
       width: 80,
       child: PageView.builder(
-        itemCount: barraMultiplicador.length,
+        itemCount: barraMultiplicadorList.length,
         scrollDirection: Axis.vertical,
         controller: PageController(viewportFraction: 0.7),
-        onPageChanged: (int index) => setState(() {
-          _index = index;
+        onPageChanged: (int index) => setStateBuilder(() {
           mIndex = index;
-          texto  = calcularResistencia();
+          calcularResistencia();
         }),
         itemBuilder: (context, i) {
           return Container(
             child: Card(
-              color: barraMultiplicador[i].color,
+              color: barraMultiplicadorList[i].color,
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10)),
               child: Center(
                 child: Text(
-                  barraMultiplicador[i].nombre,
+                  barraMultiplicadorList[i].nombre,
                   style: TextStyle(
                     fontSize: 16,
                     color: i == 0 ? Colors.white : Colors.black,
@@ -298,29 +446,27 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget _barraTolerancias( ) {
-    int _index = 0;
+  Widget barraTolerancias(Function setStateBuilder) {
     return SizedBox(
       height: 80, // card height
       width: 80,
       child: PageView.builder(
-        itemCount: barraTolerancias.length,
+        itemCount: barraToleranciasList.length,
         scrollDirection: Axis.vertical,
         controller: PageController(viewportFraction: 0.7),
-        onPageChanged: (int index) => setState(() {
-          _index   = index;
+        onPageChanged: (int index) => setStateBuilder(() {
           tolIndex = index;
-          texto    = calcularResistencia();
+          calcularResistencia();
         }),
         itemBuilder: (context, i) {
           return Container(
             child: Card(
-              color: barraTolerancias[i].color,
+              color: barraToleranciasList[i].color,
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10)),
               child: Center(
                 child: Text(
-                  barraTolerancias[i].nombre,
+                  barraToleranciasList[i].nombre,
                   style: TextStyle(
                     fontSize: 16,
                     color: i == 0 ? Colors.white : Colors.black,
---
+++ lib/src/models/resistencia.dart
@@ -0,0 +1,16 @@
+
+class Resistencia {
+  String id;
+  String valor;
+  int indexBarra1;
+  int indexBarra2;
+  int indexBarraMult;
+  int indexBarraTol;
+
+  Resistencia(
+      {this.indexBarra1,
+      this.indexBarra2,
+      this.indexBarraMult,
+      this.indexBarraTol,
+      this.valor});
+}
